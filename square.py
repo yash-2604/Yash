34549149fa3e0ba7b78a66429033afd0ecd31cb3
yash-2604 <122000240+yash-2604@users.noreply.github.com>
2023-01-05 11:13:17 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,6 @@
+x=int(input("enter a length for a square"))
+y=4*x
+a=int(input("enter a length for the radius of va circle"))
+b=4*3.14*a
+print("the perimeter of a square is",y)
+print("the perimeter of a circle is",b)
